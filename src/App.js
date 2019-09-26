6e3c3276216e1752a1fa8b440649bb6a2de92d8c
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-09-26 12:16:48 -0500

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
+import Card from "./components/Card.js";
+
 
 function App() {
-  return (
+return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+       <Card />
     </div>
   );
 }
-
 export default App;
